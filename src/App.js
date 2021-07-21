9373f409493491ad6de45f9c0dbb6887368e5b6c
Dicsylvi <dicsylvi@gmail.com>
2021-07-21 18:51:59 -0400

    Updated App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchEngine from "./SearchEngine";
 
+
 import './App.css';
 
 function App() {
   return (
     <div  className="App">
       <header className="App-header">
+     <h1>Weather App</h1>
            <SearchEngine  />
  </header>
  <small>
